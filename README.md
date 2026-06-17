40431d5b6ee08d7e48c8f06c5042c57fef695fff
CleanBlueBandit <lamaziZroxada123@gmail.com>
2026-06-17 15:52:58 +0400

    chore: ignore node_modules

---
+++ README.md
@@ -0,0 +1,14 @@
+# Among_Us_IRL
+An In real life simulation for the game "Among us", done by Node.js server-client relationship.
+
+## How its done
+Needs 3 devices to act as servers on local network. 1 is for reactor sabotage handling, 1 for oxygen, and the third for actual server logic.
+every player needs to have a mobile device which they need to carry around and complete tasks on
+
+## How do I play
+Simply download the project, install node, run npm install, and then host it your way. Dont ask me how.
+
+> *Important Note!*
+> This server is ment to be hosted on localhost only, I left several serious vulnerabilities there, because im too lazy.
+> Note that the client is completely trusted here, so if you want to play this game, just keep in mind that everyone with decent cybersecurity skills can cheat
+> (although they will need some external tools which I dont think is obtainable on mobile)
---
+++ server.js
@@ -0,0 +1,297 @@
+import express from 'express';
+import fs from 'fs/promises';
+import cookieParser from 'cookie-parser';
+import crypto from 'crypto';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+
+
+// Constants
+
+const PORT = 6767;
+const host_pass = "amogus";
+
+// Variables
+
+var timeLeft = 0
+
+// Middleware
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+
+// Start server
+
+async function loadGame() {
+    try {
+        const rawData = await fs.readFile('./game.json', 'utf-8');
+        const data = JSON.parse(rawData);
+        
+        if (!data.players) {
+            data.players = {};
+        }
+        return data;
+    } catch (error) {
+        return { players: {} };
+    }
+}
+async function saveGame(data) {
+    await fs.writeFile('./game.json', JSON.stringify(data, null, 2), 'utf-8');
+}
+
+httpServer.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+app.post('/enter', async (req, res) => {
+    try {
+        const session = req.cookies.session;
+        const data = await loadGame();
+        
+        const username = req.body.username ? String(req.body.username) : "Anonymous Crewmate";
+
+        if (session && typeof session === 'string' && data.players[session]) {
+            return res.status(200).json({message:"username accepted!"})
+        }
+
+        const UUID = crypto.randomUUID();
+        res.cookie('session', UUID, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 12 // 12 hours
+        });
+
+        const playerData = {
+            id: UUID,
+            username: username,
+            alive: true,
+            tasksCompleted: 0,
+            totalTasks: 5,
+        };
+
+        data.players[UUID] = playerData;
+        
+        await saveGame(data); 
+        
+        return res.status(200).json({message:"username created!"})
+
+    } catch (error) {
+        console.error("Error managing game entry:", error);
+        return res.status(500).json({ error: "Internal Server Error during lobby entry." });
+    }
+})
+
+
+app.get("/host", (req, res) => {
+    res.status(200).redirect("/host-login.html")
+})
+
+app.post('/enter-host', async (req, res) => {
+    try {
+        const session = req.cookies.session;
+        const data = await loadGame();
+        const username = req.body.username;
+        const password = req.body.password;
+
+        if(password != host_pass){
+            return res.status(401).json({error:"invalid credentials"})
+        }
+        
+        const UUID = crypto.randomUUID();
+        res.cookie('session', UUID, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 12 // 12 hours
+        });
+
+        const playerData = {
+            id: UUID,
+            username: username,
+            alive: true,
+            tasksCompleted: 0,
+            totalTasks: 5,
+        };
+
+        data.players[UUID] = playerData;
+        data.gameState.host = UUID;
+        
+        await saveGame(data); 
+        
+        return res.status(200).json({message:"wellcome, host!"})
+
+    } catch (error) {
+        console.error("Error managing game entry:", error);
+        return res.status(500).json({ error: "Internal Server Error during lobby entry." });
+    }
+})
+
+async function assignPlayerRoles() {
+    const data = await loadGame();
+    const playerIds = Object.keys(data.players);
+    const totalPlayers = playerIds.length;
+
+    let roleDeck = [];
+    
+    const targetImpostors = 2; 
+
+    for (let i = 0; i < totalPlayers; i++) {
+        if (i < targetImpostors) {
+            roleDeck.push("impostor");
+        } else {
+            roleDeck.push("crewmate");
+        }
+    }
+
+
+    for (let i = roleDeck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        
+        const temp = roleDeck[i];
+        roleDeck[i] = roleDeck[j];
+        roleDeck[j] = temp;
+    }
+
+
+    playerIds.forEach((id, index) => {
+        const assignedRole = roleDeck[index];
+        
+        data.players[id].impostor = (assignedRole === "impostor");
+    });
+
+    await saveGame(data);
+    
+    console.log("Roles dealt perfectly and evenly across the lobby!");
+}
+
+app.get('/dashboard', async (req, res) => {
+    const data = await loadGame();
+    const session = req.cookies.session;
+    if(!data.players[session]){
+        return res.status(401).json({error:"401 unauthorised."});
+    }
+    const playerData = data.players[session];
+
+    if(playerData.impostor){
+        return res.redirect("impostor.html");
+    }
+    return res.redirect("crewmate.html")
+})
+
+
+app.get('/logout', async(req, res) => {
+    const session = req.cookies.session;
+    const data = await loadGame();
+    if (data.players && session) {
+        if(data.gameState.host == session){
+            data.gameState.host = ""
+        }
+        delete data.players[session];
+        await saveGame(data);
+    }
+
+    res.clearCookie('session', {
+        httpOnly: true
+    });
+
+    res.redirect('/');
+})
+
+let localVisualTimer = null;
+
+app.get("/socket", (req, res) => {
+    res.redirect("/socket.html");
+})
+
+
+function parseSocketCookies(cookieHeader) {
+    const cookies = {};
+    if (!cookieHeader) return cookies;
+
+    cookieHeader.split(';').forEach(cookie => {
+        const parts = cookie.split('=');
+        const name = parts[0].trim();
+        const value = parts[1] ? parts[1].trim() : '';
+        cookies[name] = decodeURIComponent(value);
+    });
+    return cookies;
+}
+
+io.on("connection", async (socket) => {
+    const rawCookieHeader = socket.request.headers.cookie;
+    const cookies = parseSocketCookies(rawCookieHeader);
+    const data = await loadGame();
+
+    try {
+        if (data.players[cookies.session]) {
+            socket.emit("player_data_request", data.players[cookies.session]);
+            socket.emit("game_data_request", data.gameState);
+
+            let targetEndTimestamp = 0;
+            
+            if (data.activeSabotages.o2.depleted) {
+                targetEndTimestamp = Date.now() + (data.activeSabotages.o2.timeLeft * 1000);
+            } else if (data.activeSabotages.reactor.meltdown) {
+                targetEndTimestamp = Date.now() + (data.activeSabotages.reactor.timeLeft * 1000);
+            }
+            socket.emit("sabotage_data_request", { 
+                sData: data.activeSabotages, 
+                endTime: targetEndTimestamp 
+            });
+        }
+        else {
+            socket.emit("Err", { error: "username not found." });
+        }
+        
+        socket.on('disconnect', () => {
+            console.log('Client disconnected.');
+        });
+    }
+    catch (err) {
+        socket.emit("Err", { error: err.message });
+    }
+});
+
+
+app.get("/waiting", async (req, res) => {
+    const session = req.cookies.session;
+    const data = await loadGame();
+    if(session && data.players[session]){
+        if(session == data.gameState.host){
+            return res.redirect("host_lobby.html")
+        }
+        return res.redirect("waiting_lobby.html");
+    }
+    res.status(401).json({error:"401 unauthorised"})
+})
+
+app.get("/start", async (req, res) => {
+    const session = req.cookies.session;
+    const data = await loadGame();
+})
+
+let gameKillTimeout = null;
+
+function startTimestampCountdown(seconds) {
+    if (gameKillTimeout) clearTimeout(gameKillTimeout);
+
+    const msToWait = seconds * 1000;
+    const targetEndTime = Date.now() + msToWait;
+
+    io.emit("sabotage_countdown_start", { 
+        endTime: targetEndTime 
+    });
+
+    gameKillTimeout = setTimeout(async () => {
+        const data = await loadGame();
+        if (data.activeSabotages.o2.depleted) {
+            io.emit("game_over", { winner: "impostors" });
+        }
+    }, msToWait);
+}
+
